a795d774432df3f1dd5a47b0840c3bd5eeea7d6a
Xavi Alfaro <nxb.developer@gmail.com>
2024-07-10 11:52:04 -0500

    add ai set up

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
+import { mistral } from '@ai-sdk/mistral'
+import { generateText } from 'ai'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+async function main() {
+  const { text } = await generateText({
+    model: mistral('open-mixtral-8x7b'),
+    prompt: `Write a vegetarian lasagna recipe for 4 people.`,
+    maxTokens: 150
+    // messages: []
+  })
+
+  console.log(text)
+}
+
 const data = {
   title: 'SEO AI',
   description: 'Generate seo data for your website',
@@ -6,3 +23,4 @@ const data = {
 }
 
 console.log(data)
+main().catch(console.error)
