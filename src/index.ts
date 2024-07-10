335419a0d4f23b987ad41617d47f806ef432de1b
Xavi Alfaro <nxb.developer@gmail.com>
2024-07-09 23:12:05 -0500

    add initial scripts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ const data = {
   title: 'SEO AI',
   description: 'Generate seo data for your website',
   version: '0.0.1',
-  author: {
-    name: 'Xavi Alfaro',
-    url: 'https://x.com/xavimonp/'
-  },
   keywords: ['seo', 'ai', 'cli']
 }
+
+console.log(data)
